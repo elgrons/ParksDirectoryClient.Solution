e48f0c513fc10b54a2a91fc5a2450f0fcedea22e
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-31 15:55:22 -0700

    intial project build.

---
+++ ParksDirectoryClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ParksDirectoryClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
